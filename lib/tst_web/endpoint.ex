5947c7ea7253c00ec3001bdb8e34c65f6213495a
Austin <roflush@pm.me>
2020-11-12 16:43:18 -0500

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/tst_web/endpoint.ex
+++ lib/tst_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule TstWeb.Endpoint do
   ]
 
   socket "/socket", TstWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
